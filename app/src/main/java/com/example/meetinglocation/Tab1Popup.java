c13d7c0c2a38f3eaf7d1c5871ba76bd293d2718f
Jihyo Kim <wlgy33@naver.com>
2020-05-10 16:40:18 +0900

    2020.04.20 종합설계 세가지 탭 레이아웃 우선 구현 실질적 기능X, 상단 현 탭에 대한 텍스트 뷰 겹침 수정 필요
    2020.05.10
    종합설계
    구글 맵에 사용자의 위치 정보 접근권한 요청 받고
    현재 위치로 마커 생성

---
+++ app/src/main/java/com/example/meetinglocation/Tab1Popup.java
@@ -0,0 +1,47 @@
+package com.example.meetinglocation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+public class Tab1Popup extends Activity {
+    EditText addressEntered1;
+    EditText nameEntered1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab1_pop_up);
+
+        addressEntered1 = (EditText) findViewById(R.id.address_entered1);
+        nameEntered1 = (EditText) findViewById(R.id.name_entered1);
+
+
+    }
+
+    // 확인 버튼 클릭
+    public void popupClicked(View v) {
+        // 데이터 전달하기
+
+        // 팝업 닫기
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // 팝업 바깥 터치 시 안 닫힘
+        if(event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // 뒤로 가기 방지
+        return;
+    }
+}
